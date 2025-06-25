99aab48acbb73cd476509784726c57c1360cd695
Bekforever <b3kforever@gmail.com>
2025-06-25 18:23:54 +0500

    Refactor app component and add profile service
    - Removed the "goodbye world!" header from app.component.html.
    - Updated app.component.ts to inject ProfileService and fetch test accounts.
    - Added Profile interface to define the structure of profile data.
    - Created ProfileService to handle API calls for profile data.
    - Configured app.config.ts to include HttpClient for making HTTP requests.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.component";
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { Profile } from './data/interfaces/profile.interface';
+import { ProfileService } from './data/services/profile.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ProfileCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'tik-talk';
+  profileService = inject(ProfileService);
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val) => {
+      this.profiles = val;
+    });
+  }
 }
