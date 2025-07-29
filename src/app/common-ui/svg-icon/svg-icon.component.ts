c5ccc31e4e4fab113ed079e9163f7a14a9c26fb9
Bekforever <b3kforever@gmail.com>
2025-07-29 18:18:25 +0500

    Update SVG icons and sidebar styles for improved accessibility and performance
    - Added unique IDs to SVG icons for better accessibility.
    - Changed fill color of SVG paths from white to currentColor for dynamic styling.
    - Enhanced sidebar component styles to change icon color on hover, improving user interaction.

--- src/app/common-ui/svg-icon/svg-icon.component.ts
+++ src/app/common-ui/svg-icon/svg-icon.component.ts
@@ -2,14 +2,33 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'svg[icon]',
+  standalone: true,
   imports: [],
-  template: "<svg:use [attr.href]='href'></svg:use>",
-  styles: [''],
+  template: '<svg:use [attr.href]="href"></svg:use>',
+  styles: '',
 })
 export class SvgIconComponent {
-  @Input() icon = '';
+  @Input() icon: string = '';
 
+  ngOnInit() {
+    console.log("icon", this.icon)
+  }
   get href() {
-    return `/assets/svg/sprite.svg#${this.icon}`;
+    return `/assets/svg/${this.icon}.svg#${this.icon}`;
   }
 }
+// import { Component, Input } from '@angular/core';
+
+// @Component({
+//   selector: 'svg[icon]',
+//   imports: [],
+//   template: "<svg:use [attr.href]='href'></svg:use>",
+//   styles: [''],
+// })
+// export class SvgIconComponent {
+//   @Input() icon = '';
+
+//   get href() {
+//     return `/assets/svg/sprite.svg#${this.icon}`;
+//   }
+// }
